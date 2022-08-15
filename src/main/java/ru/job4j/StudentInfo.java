8dc84a416921414e787fc7ce34f081f90c26fe22
TheMoldex <themoldex@gmail.com>
2022-08-15 23:04:22 +0300

    поправил Info, Multiple, StudentInfo (фигурные скобки в методе мейн были без пробела

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Денис Сташков");
         System.out.println("23.05.92");
     }
