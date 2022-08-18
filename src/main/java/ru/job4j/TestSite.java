ca8b9756d7e580c5c252c13ec3bbed402406caab
TheMoldex <themoldex@gmail.com>
2022-08-18 13:10:49 +0300

    запуск тестов

---
+++ src/main/java/ru/job4j/TestSite.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class TestSite {
+    public static void main(String[] args) {
+        String source = "+!?";
+        String plus = "+";
+        String minus = "!";
+        String ques = "?";
+        System.out.println(plus + plus + plus);
+        System.out.println(minus + minus + minus + minus);
+        System.out.println(ques + ques);
+    }
+}
