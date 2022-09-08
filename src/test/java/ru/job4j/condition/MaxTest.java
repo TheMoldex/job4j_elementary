4f8e4ad11d10d220f8c2a4b4ea9ba411651c25ea
TheMoldex <themoldex@gmail.com>
2022-09-08 17:47:37 +0300

    4.2. Тернарное сравнение

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -14,7 +14,7 @@ class MaxTest {
     }
 
     @Test
-    public void whenMax2to1Then2(){
+    public void whenMax2to1Then2() {
         int left = 2;
         int right = 1;
         int expexted = 2;
@@ -23,7 +23,7 @@ class MaxTest {
     }
 
     @Test
-    public void whenMax2to2Then2(){
+    public void whenMax2to2Then2() {
         int left = 2;
         int right = 2;
         int expexted = 2;
