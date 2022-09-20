49e693e1f5ab7c2e10b9f1f57e6cb0debffe84be
TheMoldex <themoldex@gmail.com>
2022-09-20 13:25:45 +0300

    6.1. Классический поиск перебором

---
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FindLoopTest {
+
+    @Test
+    public void whenArrayHas5Then0() {
+        int[] data = new int[] {5, 10, 3};
+        int el = 5;
+        int result = FindLoop.indexOf(data, el);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenArrayHasNot9ThenMinus1() {
+        int[] data = new int[] {3, 5, 7, 9, 10};
+        int el = 9;
+        int expected = 4;
+        int result = FindLoop.indexOf(data, el);
+        assertThat(result).isEqualTo(expected);
+    }
+}
