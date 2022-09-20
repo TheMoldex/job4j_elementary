0594c2206da6465637411e3b79b7ca6c1b58e3cd
TheMoldex <themoldex@gmail.com>
2022-09-20 12:25:25 +0300

    6.0.3. Заполнение массива.

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + surname.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Ruslan Saburov";
+        names[1] = "Petr Arsentev";
+        names[2] = "Sergey Gervis";
+        names[3] = "Daniel Romanov";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
