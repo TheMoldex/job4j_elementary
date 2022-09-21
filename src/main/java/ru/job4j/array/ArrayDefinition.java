fdea2f7676cb7929c63ad35104ffb4b446a63ad0
TheMoldex <themoldex@gmail.com>
2022-09-21 11:15:09 +0300

    6.0.2. Размер массива

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -4,11 +4,11 @@ public class ArrayDefinition {
 
     public static void main(String[] args) {
         short[] ages = new short[10];
-        System.out.println("Размер массива равен: " + ages.length);
+        System.out.println("The array size is: " + ages.length);
         String[] surname = new String[100500];
-        System.out.println("Размер массива равен: " + surname.length);
+        System.out.println("The array size is: " + surname.length);
         float[] prices = new float[40];
-        System.out.println("Размер массива равен: " + prices.length);
+        System.out.println("The array size is: " + prices.length);
         String[] names = new String[4];
         names[0] = "Ruslan Saburov";
         names[1] = "Petr Arsentev";
